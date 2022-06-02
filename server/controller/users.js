6f2129c4c2bfd9c82dcd3403084e6a7a264a47ec
abhithapa <71200724+abhithapa@users.noreply.github.com>
2022-06-02 17:43:18 +0545

    creating user profile

--- server/controller/users.js
+++ server/controller/users.js
@@ -2,132 +2,60 @@ const userModel = require("../models/users");
 const bcrypt = require("bcryptjs");
 
 class User {
-  async getAllUser(req, res) {
-    try {
-      let Users = await userModel
-        .find({})
-        .populate("allProduct.id", "pName pImages pPrice")
-        .populate("user", "name email")
-        .sort({ _id: -1 });
-      if (Users) {
-        return res.json({ Users });
-      }
-    } catch (err) {
-      console.log(err);
-    }
-  }
+  try{
+    const token=req.headers.authorization.split(" ")[1];
+    const data=jwt.verify(token, "mysecretkey");
 
-  async getSingleUser(req, res) {
-    let { uId } = req.body;
-    if (!uId) {
-      return res.json({ error: "All filled must be required" });
-    } else {
-      try {
-        let User = await userModel
-          .findById(uId)
-          .select("name email phoneNumber userImage updatedAt createdAt");
-        if (User) {
-          return res.json({ User });
-        }
-      } catch (err) {
-        console.log(err);
-      }
-    }
-  }
+    customer.findOne({_id:data.customerId}).then(function(result){
+        console.log(result)
+        req.customerInfo=result;
+        next();
+    })
+    .catch(function(e){
+        res.json({err:e})
+    })
+}
+catch(e){
+    res.send({msg:"Invalid token"})
+}
+   
+}
 
-  async postAddUser(req, res) {
-    let { allProduct, user, amount, transactionId, address, phone } = req.body;
-    if (
-      !allProduct ||
-      !user ||
-      !amount ||
-      !transactionId ||
-      !address ||
-      !phone
-    ) {
-      return res.json({ message: "All filled must be required" });
-    } else {
-      try {
-        let newUser = new userModel({
-          allProduct,
-          user,
-          amount,
-          transactionId,
-          address,
-          phone,
-        });
-        let save = await newUser.save();
-        if (save) {
-          return res.json({ success: "User created successfully" });
-        }
-      } catch (err) {
-        return res.json({ error: error });
-      }
+try{
+    module.exports.verifyDoner=function(req, res, next){
+        const token = req.headers.authorization.split(" ")[1];
+        const data=jwt.verify(token, "mysecretkey");
+        doner.findOne({_id:data.donerId}).then(function(result){
+            req.donerInfo=result;
+            // console.log(result);
+            next();
+        })
+        .catch(function(e){
+            res.json({err:e})
+        })
     }
-  }
-
-  async postEditUser(req, res) {
-    let { uId, name, phoneNumber } = req.body;
-    if (!uId || !name || !phoneNumber) {
-      return res.json({ message: "All filled must be required" });
-    } else {
-      let currentUser = userModel.findByIdAndUpdate(uId, {
-        name: name,
-        phoneNumber: phoneNumber,
-        updatedAt: Date.now(),
-      });
-      currentUser.exec((err, result) => {
-        if (err) console.log(err);
-        return res.json({ success: "User updated successfully" });
-      });
+}
+catch{
+    res.json({err:"error"});
+}
+try{
+    module.exports.verifyOrganisation=function(req, res, next){
+        const token=req.headers.authorization.split(" ")[1];
+        const data=jwt.verify(token, "mysecretkey");
+    
+        organization.findOne({_id:data.organizationId}).then(function(result){
+            res.organizationInfo=result;
+            next();
+        }).catch(function(e){
+            res.json({err:e});
+        })
+        
+        
     }
-  }
-
-  async getDeleteUser(req, res) {
-    let { oId, status } = req.body;
-    if (!oId || !status) {
-      return res.json({ message: "All filled must be required" });
-    } else {
-      let currentUser = userModel.findByIdAndUpdate(oId, {
-        status: status,
-        updatedAt: Date.now(),
-      });
-      currentUser.exec((err, result) => {
-        if (err) console.log(err);
-        return res.json({ success: "User updated successfully" });
-      });
-    }
-  }
-
-  async changePassword(req, res) {
-    let { uId, oldPassword, newPassword } = req.body;
-    if (!uId || !oldPassword || !newPassword) {
-      return res.json({ message: "All filled must be required" });
-    } else {
-      const data = await userModel.findOne({ _id: uId });
-      if (!data) {
-        return res.json({
-          error: "Invalid user",
-        });
-      } else {
-        const oldPassCheck = await bcrypt.compare(oldPassword, data.password);
-        if (oldPassCheck) {
-          newPassword = bcrypt.hashSync(newPassword, 10);
-          let passChange = userModel.findByIdAndUpdate(uId, {
-            password: newPassword,
-          });
-          passChange.exec((err, result) => {
-            if (err) console.log(err);
-            return res.json({ success: "Password updated successfully" });
-          });
-        } else {
-          return res.json({
-            error: "Your old password is wrong!!",
-          });
-        }
-      }
-    }
-  }
+}
+catch{
+    res.json({err:"err"})
+}
 }
 
 const ordersController = new User();
