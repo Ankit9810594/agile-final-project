aff4ecfd31f62df2d880e01c1a6b07cc9e47ec1c
abhithapa <71200724+abhithapa@users.noreply.github.com>
2022-07-04 12:32:34 +0545

    final testing for bdd

--- test-app-bdd/features/step_definations/index.js
+++ test-app-bdd/features/step_definations/index.js
@@ -3,13 +3,13 @@ const { Given, When, Then, Before, After } = require("@cucumber/cucumber");
 const { Builder, By, Key, until, sleep } = require("selenium-webdriver");
 const { delay } = require("../utils/delay");
 
-Given("Test registration functionality", { timeout: 30000 }, async function () {
+Given("Testing functionality of register", { timeout: 30000 }, async function () {
   let driver = await new Builder().forBrowser("chrome").build();
   await driver.get("http://localhost:3000/signup");
-  await driver.findElement(By.id("name")).sendKeys("test");
-  await driver.findElement(By.id("email")).sendKeys("test@gmail.com");
-  await driver.findElement(By.id("password")).sendKeys("test1234");
-  await driver.findElement(By.id("cpassword")).sendKeys("test1234");
+  await driver.findElement(By.id("name")).sendKeys("Abhishek");
+  await driver.findElement(By.id("email")).sendKeys("abhishek@gmail.com");
+  await driver.findElement(By.id("password")).sendKeys("abhishek1234");
+  await driver.findElement(By.id("cpassword")).sendKeys("abhishek1234");
 
   await driver.sleep(delay);
   await driver.findElement(By.id("registerBtn")).click();
@@ -21,8 +21,8 @@ Given("Test registration functionality", { timeout: 30000 }, async function () {
 Given("Test login functionality", { timeout: 30000 }, async function () {
   let driver = await new Builder().forBrowser("chrome").build();
   await driver.get("http://localhost:3000/");
-  await driver.findElement(By.id("email")).sendKeys("test@gmail.com");
-  await driver.findElement(By.id("password")).sendKeys("test1234");
+  await driver.findElement(By.id("email")).sendKeys("abhishek@gmail.com");
+  await driver.findElement(By.id("password")).sendKeys("abhishek1234");
   await driver.sleep(delay);
   await driver.findElement(By.id("loginBtn")).click();
 
