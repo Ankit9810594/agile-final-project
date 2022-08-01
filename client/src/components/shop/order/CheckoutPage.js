dc5177298b1274f44428a354cf51b1d242910f20
divyacmd <100414040+divyacmd@users.noreply.github.com>
2022-08-01 14:04:22 +0545

    Update CheckoutPage.js

--- client/src/components/shop/order/CheckoutPage.js
+++ client/src/components/shop/order/CheckoutPage.js
@@ -1,13 +1,1 @@
-import React, { Fragment } from "react";
-import Layout from "../layout";
-import { CheckoutComponent } from "./CheckoutProducts";
 
-const CheckoutPage = (props) => {
-  return (
-    <Fragment>
-      <Layout children={<CheckoutComponent />} />
-    </Fragment>
-  );
-};
-
-export default CheckoutPage;
